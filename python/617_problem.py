7ba62c87cac9e41e50f9b14253838b0bfd701498
snowmanunderwater <snowmanunderwater@gmail.com>
2020-02-15 06:44:38 +0300

    solve '617' problem

---
+++ python/617_problem.py
@@ -0,0 +1,57 @@
+# 617. Merge Two Binary Trees
+# https://leetcode.com/problems/merge-two-binary-trees/
+
+"""Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+
+You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+
+Example 1:
+
+Input: 
+	Tree 1                     Tree 2
+
+          1                         2                             
+         / \                       / \                            
+        3   2                     1   3                        
+       /                           \   \                      
+      5                             4   7  
+
+Output: 
+Merged tree:
+	     3
+	    / \
+	   4   5
+	  / \   \ 
+	 5   4   7
+ 
+
+Note: The merging process must start from the root nodes of both trees.
+"""
+
+
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+    
+    def __repr__(self):
+        return f"val:{self.val} l:{self.left} r:{self.right}"
+
+
+def mergeTrees(t1: TreeNode, t2: TreeNode) -> TreeNode:
+    if t1 is None or t2 is None:
+        pass
+
+
+t1 = TreeNode(1)
+t1.left = TreeNode(3)
+t1.left.left = TreeNode(5)
+t1.right = TreeNode(2)
+t1.right.right = TreeNode(4)
+
+t2 = TreeNode(2)
+t2.left = TreeNode(1)
+t2.left.right = TreeNode(4)
+t2.right = TreeNode(3)
+t2.right.right = TreeNode(7)

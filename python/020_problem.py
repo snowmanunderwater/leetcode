005c758c7a291c12e7fe589cecb6432db2edaa34
snowmanunderwater <snowmanunderwater@gmail.com>
2020-03-14 07:01:11 +0300

    solve '20' problem

---
+++ python/020_problem.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/valid-parentheses/
+
+def check_brackets(stroka):
+	acc = []
+
+	for char in stroka:
+		if char in ')]}' and len(acc) == 0:
+			return False
+		if char in '({[':
+			acc.append(char)
+		elif char in ')]}' and len(acc) > 0:
+			if char == ')' and acc[-1] != '(':
+				return False
+			elif char == ']' and acc[-1] != '[':
+				return False
+			elif char == '}' and acc[-1] != '{':
+				return False
+			acc.pop()
+
+	return False if acc else True
+
+
+assert check_brackets(')(') == False
+assert check_brackets('(()') == False
+assert check_brackets('({)()})') == False
+assert check_brackets('(3+[2*3)]') == False
+assert check_brackets(']') == False
+assert check_brackets('[]') == True
+assert check_brackets('[([])]') == True
+assert check_brackets('(5+5)/[4+4]*{2*2}') == True
